fe2bf2eba82874ff77e611a944dba1ee6715851d
Your Name <you@example.com>
2024-06-03 15:59:37 +0200

    first commit project

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppHeader from './components/AppMain.vue'
+import AppHeader from './components/AppFooter.vue'
+import { store } from "./store";
+
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+
+  created() {
+   
+  },
+  
+  methods: {
+  
+      }
+    }
+
+
+</script>
+
+
+
+
+
+
+<template>
+
+
+
+
+
+
+
+</template>
