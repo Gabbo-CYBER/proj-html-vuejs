30937ef586c71bff7f1a1cc473638f1fc45cc7e2
Your Name <you@example.com>
2024-06-04 18:11:46 +0200

    commit 2day

--- src/components/Jumbtron.vue
+++ src/components/Jumbtron.vue
@@ -14,15 +14,22 @@
 <template>
     <div class="card mb-3">
         <div class="row g-0 align-items-center">
-            <div class="col-5 text-cemter">
-                <p class="card-blue text-center">17 years of experiance</p>
+            <div class="left-col  col-4 text-cemter">
+                <p class="green text-center">17 years of experiance</p>
                 <h1 class="card-title text-center">We Are a</h1>
-                <h1 class="card-title text-center">Web Design Agency</h1>
-                <p class="card-blue text-center">wfwefwe</p>
-                <p class="red text-center">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,there live the blind texts. Separated they live in Bookmarksgrove right at the coast of theSemantics, a large language ocean.Separated they live in Bookmarksgrove.</p>
-                <p class="card-blue text-center m-5"><button type="button" class="btn btn-primary">READ More</button>
+                <h1 class="card-title text-center">Web Design <span class="Agency">Agency</span></h1>
+                <hr class="d-inline-block me-2 rounded bg-success opacity-100" />
+                <hr class="separator d-inline-block rounded bg-success opacity-100" />
+                <p class="description-card text-center">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,there live the blind texts. Separated they live in Bookmarksgrove right at the coast of theSemantics, a large language ocean.Separated they live in Bookmarksgrove.</p>
+                <p class="black-card text-start m-5"><button class="btn rounded-5 text-white px-5 py-3 shadow-lg mt-4 fs-6">READ MORE</button>
                 </p>
-                <p class="card-blue text-center">ICONE</p>
+                <span>
+                    <a class="click link-body-emphasis link-offset-2 link-underline link-underline-opacity-0 px-5 py-3 fs-6" href="#">Facebook</a>
+                    <a class="click link-body-emphasis link-offset-2 link-underline link-underline-opacity-0 px-5 py-3 fs-6" href="#">Instagram</a>
+                    <a class="click link-body-emphasis link-offset-2 link-underline link-underline-opacity-0 px-5 py-3 fs-6" href="#">Twitter</a>
+                    <a class="click link-body-emphasis link-offset-2 link-underline link-underline-opacity-0 px-5 py-3 fs-6" href="#">Youtube</a>
+
+                </span>
             </div>
             <div class="col-7">
                 <div class="card-body">
@@ -44,11 +51,86 @@
 <style scoped>
 .prova {
     width: 100%;
-    
 }
 
-.red{
-    
+.left-col{
+    margin-left: 8rem;
 }
 
+
+.green{
+    font-size: 23px;
+    color: #00d9a6;
+    font-family: quicksand;
+    font-weight: 500;
+    line-height: 120%;
+    letter-spacing: 7px;
+    background-color: transparent;
+}
+
+h1{
+    font-size: 72px;
+    color: #202020;
+    font-family: cursive;
+    font-weight: 700;
+    line-height: 120%;
+    letter-spacing: -4px;
+    background-color: transparent;
+
+}
+
+.Agency{
+    font-size: 72px;
+    color: #00d9a6;
+    font-family: cursive;
+    font-weight: 700;
+    line-height: 120%;
+    letter-spacing: -4px;
+    background-color: transparent;
+}
+
+
+hr {
+    width: 0.8rem;
+    padding: 0.2rem 0;
+    border: 0;
+}
+
+.separator {
+    width: 3rem;
+    padding: 0.2rem 0;
+    border: 0;
+}
+
+.description-card {
+    font-size: 15px;
+    font-family: ubuntu;
+    font-weight: 300;
+    line-height: 160%;
+    background-color: transparent;
+}
+
+
+
+button {
+    font-size: 0.6rem;
+    display: inline-block;
+    position: relative;
+    background-color: black;
+    transition: color 0.5s;
+    z-index: 1;
+    overflow: hidden;
+
+   
+}
+
+button:hover {
+  background-color: #00d9a6;
+}
+
+
+.click{
+    overflow: auto;
+    background-color: ;
+}
 </style>
--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,4 +1,19 @@
 <script>
+export default {
+  name: 'LinkList',
+  data() {
+    return {
+      links: [
+        { url: '#home', home: 'HOME' },
+        { url: '#ourMissions', about: 'ABOUT' },
+        { url: '#ourWorks', services: 'SERVICES' },
+        { url: '#ourResults', showcase: 'SHOWCASE' },
+        { url: '#latestNews', blog: 'BLOG' },
+        { url: '#contactInfo', contact: 'CONTACT' },
+      ]
+    };
+  }
+};
 
 
 
@@ -7,26 +22,26 @@
 
 
 <template>
-    <ul class="nav justify-content-center">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-        </ul>
+  <ul class="nav justify-content-center">
+    <li class="nav-item m-1" v-for="(link, index) in links" :key="index">
+      <a class="link-body-emphasis link-offset-2 link-underline link-underline-opacity-0" :href="link.url" target="_blank">{{ link.home }}</a>
+    </li>
+    <li class="nav-item m-1" v-for="(link, index) in links" :key="index">
+      <a class=" link-body-emphasis link-offset-2 link-underline link-underline-opacity-0" :href="link.url" target="_blank">{{ link.about }}</a>
+    </li>
+    <li class="nav-item m-1" v-for="(link, index) in links" :key="index">
+      <a class="link-body-emphasis link-offset-2 link-underline link-underline-opacity-0":href="link.url" target="_blank">{{ link.services }}</a>
+    </li>
+    <li class="nav-item m-1" v-for="(link, index) in links" :key="index">
+      <a class="link-body-emphasis link-offset-2 link-underline link-underline-opacity-0":href="link.url" target="_blank">{{ link.showcase }}</a>
+    </li>
+    <li class="nav-item m-1" v-for="(link, index) in links" :key="index">
+      <a class=" link-body-emphasis link-offset-2 link-underline link-underline-opacity-0":href="link.url" target="_blank">{{ link.blog }}</a>
+    </li>
+    <li class="nav-item m-1" v-for="(link, index) in links" :key="index">
+      <a class="link-body-emphasis link-offset-2 link-underline link-underline-opacity-0":href="link.url" target="_blank">{{ link.contact }}</a>
+    </li>
+  </ul>
 
 
 
@@ -38,8 +53,9 @@
 
 <style scoped>
 
-
-
+.nav-item{
+  list-style: none;
+}
 
 
 </style>
